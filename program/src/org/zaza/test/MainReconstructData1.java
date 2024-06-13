3da3b6ac4ddbf787d54bd909ce2287750452c486
sitiazzalea <sitiazzaleaannisa@gmail.com>
2024-06-14 04:55:22 +0700

    testing again

--- program/src/org/zaza/test/MainReconstructData1.java
+++ program/src/org/zaza/test/MainReconstructData1.java
@@ -6,17 +6,17 @@ import org.zaza.utils.HelperTools;
 
 public class MainReconstructData1 {
     public static void main(String[] args) throws Exception {
-        File secret = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley - 931.png");
+        File secret = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley.png");
 
-        File input1 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\smiley - 931_2.PRESS");
-        File input2 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\smiley - 931_4.PRESS");
-        File input3 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\smiley - 931_7.PRESS");
-        File input4 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\smiley - 931_6.PRESS");
-        File input5 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\smiley - 931_3.PRESS");
+        File input1 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5_2.PRESS");
+        File input2 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5_4.PRESS");
+        File input3 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5_6.PRESS");
+        File input4 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5_7.PRESS");
+        File input5 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5_1.PRESS");
 
         File[] inputs = {input1, input2, input3, input4, input5};
 
-        File hasilReconstruct = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-931-P3t4\\HasilRekonstruksi_smiley - 931-P3t4.PRESS");
+        File hasilReconstruct = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\smiley - P4t5-p3.png");
 
         FileHandler.writeFileFromShares(inputs, hasilReconstruct);
 
--- program/src/org/zaza/test/MainReconstructData2.java
+++ program/src/org/zaza/test/MainReconstructData2.java
@@ -6,17 +6,17 @@ import org.zaza.utils.HelperTools;
 
 public class MainReconstructData2 {
     public static void main(String[] args) throws Exception {
-        File secret = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565.png");
+        File secret = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652.png");
 
-        File input1 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565-P3t4_1.PRESS");
-        File input2 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565-P3t4_5.PRESS");
-        File input3 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565-P3t4_6.PRESS");
-        File input4 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565-P3t4_2.PRESS");
-        File input5 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-3565-P3t4_4.PRESS");
+        File input1 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652_1.PRESS");
+        File input2 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652_6.PRESS");
+        File input3 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652_4.PRESS");
+        File input4 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652_3.PRESS");
+        File input5 = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\smiley-4652_5.PRESS");
 
         File[] inputs = {input1, input2, input3, input4, input5};
 
-        File hasilReconstruct = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\Hasil Rekonstruksi-smiley-3565-P3t4.png");
+        File hasilReconstruct = new File("C:\\Users\\Azzalea\\Documents\\JavaProject\\TugasAkhir\\program\\src\\org\\zaza\\test\\usedTestCase\\HasilRekonstruksi-smiley-4652-P4t5-p5.png");
 
         FileHandler.writeFileFromShares(inputs, hasilReconstruct);
 
